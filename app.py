b5a42a38017fe5909356aa76626a1021d37358c9
Adrien Allard <allard15@hotmail.fr>
2025-02-11 14:28:22 +0100

    First commit

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# Gérer l'événement de vote
+@socketio.on('vote')
+def handle_vote(vote_data):
+    print(f"Vote reçu : {vote_data}")
+    # Diffuser le vote à tous les clients connectés
+    emit('new_vote', vote_data, broadcast=True)
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
